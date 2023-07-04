e880f408f0d06508a9c751f5a32a0498189cee02
Nacho Menéndez <nchmnndzlpz@gmail.com>
2023-07-04 23:56:02 +0200

    Update part 3 notes backend

--- models/note.js
+++ models/note.js
@@ -1,30 +1,28 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
-mongoose.set("strictQuery", false);
+mongoose.set("useFindAndModify", false);
 
 const url = process.env.MONGODB_URI;
 
-console.log("connecting to", url);
+console.log("commecting to", url);
 
 mongoose
-  .connect(url)
+  .connect(url, { useNewUrlParser: true })
   .then((result) => {
     console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
+    console.log("error connection to MongoDB:", error.message);
   });
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
+    required: true,
     minlength: 5,
-    required: true
   },
-  date: { 
-    type: Date,
-    required: true
-  },
+  date: Date,
   important: Boolean,
 });
 
